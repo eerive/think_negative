6e82dea24bae1fb37bf11c29e95d678fc3a8c7de
Glueckseule <30928455+Glueckseule@users.noreply.github.com>
2017-08-14 17:24:03 +0200

    Merge branch 'master' of https://github.com/eerive/think_negative into glueckseule_changes
    # Conflicts:
    #	app/src/main/java/ss17/droid/unir/thinknegative/StartScreenActivity.java

--- app/src/main/java/ss17/droid/unir/thinknegative/StartScreenActivity.java
+++ app/src/main/java/ss17/droid/unir/thinknegative/StartScreenActivity.java
@@ -11,6 +11,5 @@ public class StartScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
-
     }
 }
