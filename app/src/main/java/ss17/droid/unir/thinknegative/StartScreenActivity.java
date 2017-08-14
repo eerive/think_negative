c2f85d4ded5f6f833bcf446fff2f4d32f86d5a33
eerive <devil188ongl@gmail.com>
2017-08-14 16:23:03 +0200

    Testing a number

--- app/src/main/java/ss17/droid/unir/thinknegative/StartScreenActivity.java
+++ app/src/main/java/ss17/droid/unir/thinknegative/StartScreenActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class StartScreenActivity extends AppCompatActivity {
 
+    private int testNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
