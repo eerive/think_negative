c385ca0b021d70b7d6e513572b5a3e9656cd07f9
eerive <devil188ongl@gmail.com>
2017-08-14 16:17:01 +0200

    Default commit.

---
+++ app/src/main/java/ss17/droid/unir/thinknegative/StartScreenActivity.java
@@ -0,0 +1,13 @@
+package ss17.droid.unir.thinknegative;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
