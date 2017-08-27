38ed64baa42ade809b3a9f315541b708168041f2
eerive <devil188ongl@gmail.com>
2017-08-27 17:51:22 +0200

    created a menuItem, incomplete - inProgress

--- app/src/main/java/ss17/droid/unir/thinknegative/StartScreenActivity.java
+++ app/src/main/java/ss17/droid/unir/thinknegative/StartScreenActivity.java
@@ -3,17 +3,23 @@ package ss17.droid.unir.thinknegative;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class StartScreenActivity extends AppCompatActivity {
+
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_mask);
-
-
         // Init intro slide screen, only the first launch
         Thread t = new Thread(new Runnable() {
             @Override
@@ -29,10 +35,19 @@ public class StartScreenActivity extends AppCompatActivity {
             }
         });
         t.start();
+        //---------------END INTRO------------------------------------------------------------------
+        //incomplete ActionBar
+        //tut:https://www.youtube.com/watch?v=AS92bq3XxkA
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open,R.string.close);
 
-        //
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
 
-
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        //--------------END ACTIONBAR---------------------------------------------------------------
     }
-
 }
