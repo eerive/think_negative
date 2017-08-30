50843c87cef2b3331e0f2672aba6cf9dbf91b0f7
Glueckseule <30928455+Glueckseule@users.noreply.github.com>
2017-08-30 15:12:38 +0200

    Initialisierung UI

--- app/src/main/java/ss17/droid/unir/thinknegative/StartScreenActivity.java
+++ app/src/main/java/ss17/droid/unir/thinknegative/StartScreenActivity.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
@@ -17,6 +18,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
 
@@ -26,6 +31,18 @@ public class StartScreenActivity extends AppCompatActivity implements OnClickLis
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     private Toolbar mToolbar;
+
+    private ImageButton mUnicornButton;
+    private ImageButton mBaseballbatButton;
+    private ImageButton mExplosionButton;
+    private ImageButton mCowButton;
+    private ImageButton mShitButton;
+    private ImageButton mPenguinButton;
+
+    private EditText mInput;
+
+    private ImageView mImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,6 +105,55 @@ public class StartScreenActivity extends AppCompatActivity implements OnClickLis
                 return true;
             }
         });
+
+        initUI();
+
+    }
+
+    private void initUI() {
+        mUnicornButton = (ImageButton) findViewById(R.id.button_unicorn);
+        mUnicornButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mBaseballbatButton = (ImageButton) findViewById(R.id.button_bat);
+        mBaseballbatButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mCowButton = (ImageButton) findViewById(R.id.button_cow);
+        mCowButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mExplosionButton = (ImageButton) findViewById(R.id.button_explosion);
+        mExplosionButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mShitButton = (ImageButton) findViewById(R.id.button_shit);
+        mShitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mPenguinButton = (ImageButton) findViewById(R.id.button_penguin);
+        mPenguinButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mImageView = (ImageView) findViewById(R.id.fotoView);
     }
 
     @Override
